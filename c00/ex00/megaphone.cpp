ef97f227ad15ba72a297bb1d2c50ce1bd98fb81d
jeanoza <choikb916@gmail.com>
2022-07-26 23:21:17 +0200

    fix(c00/ex00): use another way

--- c00/ex00/megaphone.cpp
+++ c00/ex00/megaphone.cpp
@@ -12,14 +12,18 @@
 
 #include <iostream>
 
-//prototype: https://en.cppreference.com/w/cpp/algorithm/transform
+//transform: https://en.cppreference.com/w/cpp/algorithm/transform - is it possible in c++98?
+//iterator: https://cplusplus.com/reference/string/string/begin/
 int main(int ac, char *av[])
 {
-	for(int i = 1; i < ac; ++i)
-	{
+	for(int i = 1; i < ac; ++i) {
 		std::string current = av[i];
-		std::transform(current.begin(), current.end(), current.begin(), ::toupper);
-		std::cout << current;
+		// std::transform(current.begin(), current.end(), current.begin(), toupper);
+		// std::cout << current;
+		for (std::string::iterator it = current.begin(); it != current.end(); ++it) {
+			std::cout << char(toupper(*it));
+		}
+
 	}
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
