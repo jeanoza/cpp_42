25622de9988dfce45165a7c65ec9a6cf0d3691be
jeanoza <choikb916@gmail.com>
2022-07-30 23:29:08 +0200

    fix(functional)

--- c00/ex01/Contact.cpp
+++ c00/ex01/Contact.cpp
@@ -76,36 +76,53 @@ void	Contact::add(int *index) {
 		"Darkest secret",
 	};
 	std::string tmp;
-	for (int i = 0; i < 5; i++) {
-		std::cout << entries[i] << ": ";
+	int i = 0;
+	while (i < 5) {
+		std::cout << CYN << entries[i] << ": " << DFT;
 		getline(std::cin, tmp);
 		if (i == 0) this->setFirstName(tmp);
 		else if(i == 1) this->setLastName(tmp);
 		else if(i == 2) this->setNickName(tmp);
 		else if(i == 3) this->setPhoneNumber(tmp);
 		else if(i == 4) this->setDarkestSecret(tmp);
+		if (tmp.length()) ++i;
+		else std::cout << RED << "Each field cannot be empty!\n" << DFT << std::endl;
 	}
-	std::cout << "Registered" << std::endl << std::endl;
+	std::cout << YEL << "\n- - - - - - - - - - - - - - - - - -\n"
+			<< "Registered in index " << *index << "\n"
+			<< "- - - - - - - - - - - - - - - - - -"
+			<< DFT << std::endl;
 	this->isEmpty = false;
 	*index = (*index + 1) % MAX_CONTACT;
 }
 
+void	hello (std::string str) {
+	int space = 10 - str.length();
+	for (int i = 0; i < space; ++i) std::cout << " ";
+	if (space <= 0) std::cout << str.substr(0, 9) << '.';
+	else std::cout << str;
+}
+
 void	Contact::show(void) {
 	if (this->isEmpty == false) {
-		std::cout << this->getIndex() << "|";
-		std::cout << this->getFirstName() << "|";
-		std::cout << this->getLastName() << "|";
-		std::cout << this->getNickName() << "|" << std::endl << std::endl;
+		std::cout << "     " << this->getIndex() << " | ";
+		hello(this->getFirstName());
+		std::cout << " | ";
+		hello(this->getLastName());
+		std::cout << " | ";
+		hello(this->getNickName());
+		std::cout << std::endl;
 	}
 }
 
 void	Contact::showDetail(void) {
+	std::cout << "\n- - - - - - - - - - - - - - - - - -" << std::endl;
 	std::cout << "First name: "<< this->getFirstName() << std::endl;
 	std::cout << "Last name: "<< this->getLastName() << std::endl;
 	std::cout << "Nick name: "<< this->getNickName() << std::endl;
 	std::cout << "Phone number: "<< this->getPhoneNumber() << std::endl;
 	std::cout << "Darkest secret: "<< this->getDarkestSecret() << std::endl;
-	std::cout << "==================" << std::endl << std::endl;
+	std::cout << "- - - - - - - - - - - - - - - - - -" << std::endl;
 }
 
 int Contact::_length = 0;
--- c00/ex01/main.cpp
+++ c00/ex01/main.cpp
@@ -6,32 +6,52 @@
 /*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 17:59:48 by kyubongchoi       #+#    #+#             */
-/*   Updated: 2022/07/30 18:42:59 by kyubongchoi      ###   ########.fr       */
+/*   Updated: 2022/07/30 23:28:59 by kyubongchoi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
+bool isNumber(const std::string s)
+{
+    for (int i = 0; s[i]; ++i) {
+		char ch = s[i];
+        if (std::isdigit(ch) == 0)
+            return false;
+    }
+    return true;
+}
 
 static void search (Contact list[MAX_CONTACT]) {
 	if (list[0].isEmpty) {
-		std::cout << "Contact is empty!" << std::endl;
+		std::cout << RED << "Contact is empty!\n" << DFT <<std::endl;
 		return ;
-	}
+	} 
+	std::cout << " index | first name | last name  |  nickname   \n"
+				<< "- - - - - - - - - - - - - - - - - - - - - - - -" << std::endl;
+
 	for (int j = 0; j < MAX_CONTACT; ++j) list[j].show();
 
-	std::cout << "Chose a index whose you want see the detail: ";
+	//TODO: maybe with while loop, if invalid index => ask encore.
+	std::cout << MAG << "\nChoose a index whose you want see the detail: " << DFT;
 	std::string option;
 	getline(std::cin, option);
 
-	int index = atoi(option.c_str());
-	if (index >= 0 && index < MAX_CONTACT && !list[index].isEmpty) list[index].showDetail();
-	else std::cout << "This index is not valid!" << std::endl;
+	if (isNumber(option)) {
+		int index = atoi(option.c_str());
+		if (index >= 0 && index < MAX_CONTACT && !list[index].isEmpty) list[index].showDetail();
+		else std::cout << RED <<"This index is not valid!\n" << DFT <<std::endl;
+	}
+	else std::cout << RED <<"This index is not valid!\n" << DFT <<std::endl;
 }
 int main(void) {
 	Contact		list[MAX_CONTACT];
 
-	std::cout << "Welcome 42 Contact!" << std::endl;
+	std::cout << BLU
+			<< "\n- - - - - - - - - - - - - - - - - - - - - - - -\n"
+			<< "|             Welcome 42 Contact!             |\n"
+			<< "- - - - - - - - - - - - - - - - - - - - - - - -"
+			<< DFT <<std::endl;
 	/*
 	 * DO NOT USE getline and cin >> at the same time in program
 	 * It will waste buffer and program won't work as I want
@@ -40,12 +60,10 @@ int main(void) {
 	std::string	option;
 	while (42)
 	{
-		std::cout << "Option: ";
+		std::cout << GRN <<"\nOption: " << DFT;
 		getline(std::cin, option);
-		if (option.compare("ADD") == 0) {
-			list[i].add(&i);
-			// i = (i + 1) % MAX_CONTACT;
-		}
+		std::cout << std::endl;
+		if (option.compare("ADD") == 0) list[i].add(&i);
 		else if (option.compare("SEARCH") == 0) search(list);
 		else if (option.compare("EXIT") == 0) break ;
 	}
