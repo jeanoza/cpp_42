cb271f34e17faf12e0e3d6da2b02f0ce43e08abe
jeanoza <choikb916@gmail.com>
2022-07-31 18:48:29 +0200

    feat(c01/ex02)

--- c01/ex02/main.cpp
+++ c01/ex02/main.cpp
@@ -5,21 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/31 15:05:56 by kyubongchoi       #+#    #+#             */
-/*   Updated: 2022/07/31 18:19:16 by kyubongchoi      ###   ########.fr       */
+/*   Created: 2022/07/31 18:20:38 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/07/31 18:48:09 by kyubongchoi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <iostream>
 
 int	main(void) {
-	Zombie *list = zombieHorde(10, "testZombie");
 
-	/* Test to see if all zombies have a name and announce work well */
-	for (int i = 0; i < Zombie::getSize(); ++i) list[i].announce();
+	std::string _string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &_string;
+	std::string &stringREF = _string;
 
-	/* Destroy all zombies */
-	delete [] list;
+	std::cout << "_string's address:	" << &_string << std::endl;
+	std::cout << "stringPTR's address:	" << stringPTR << std::endl;
+	std::cout << "stringREF's address:	" << &stringREF << std::endl;
+
+	std::cout << "= = = = = = = = = = = = = = = = = = = = = = = =\n";
+
+	std::cout << "_string's value:	" << _string << std::endl;
+	std::cout << "stringPTR's value:	" << *stringPTR << std::endl;
+	std::cout << "stringREF's value:	" << stringREF << std::endl;
 
 	return (0);
 }
