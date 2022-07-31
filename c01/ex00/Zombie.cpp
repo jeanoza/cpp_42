a66c32cdebacbf8530fc4c55408d98932e49c7b9
jeanoza <choikb916@gmail.com>
2022-07-31 15:45:03 +0200

    fix(c01/ex00): put color

--- c01/ex00/Zombie.cpp
+++ c01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 15:06:34 by kyubongchoi       #+#    #+#             */
-/*   Updated: 2022/07/31 15:36:26 by kyubongchoi      ###   ########.fr       */
+/*   Updated: 2022/07/31 15:44:43 by kyubongchoi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ Zombie::Zombie(void) {
 	std::cout << "Constructor called" << std::endl;
 }
 Zombie::Zombie(std::string name) : _name(name) {
-	std::cout << "Constructor with name called"
-		<< ": " << this->_name << std::endl;
+	std::cout << BLU << "Constructor with name called"
+		<< ": " << this->_name << DFT << std::endl;
 }
 
 /* Destructor */
 Zombie::~Zombie(void) {
-	std::cout << "Destructor called"
-		<< ": " << this->_name << std::endl;
+	std::cout << RED << "Destructor called"
+		<< ": " << this->_name << DFT << std::endl;
 	return ;
 }
 
