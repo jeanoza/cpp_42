2bbf8c2966f3bd368d10cac3274b42ea778d2d98
Kyubong CHOI <kychoi@e1r6p17.clusters.42paris.fr>
2022-08-07 21:09:03 +0200

    go to home

--- c01/ex04/main.cpp
+++ c01/ex04/main.cpp
@@ -13,98 +13,49 @@
 #include <iostream>
 #include <fstream>
 
-/* Test find and its family functions
-std::cout << "find          :" << data.find(av[2]) << std::endl;
-std::cout << "first_of      :" << data.find_first_of(av[2]) << std::endl;
-std::cout << "first_not_of  :" << data.find_first_not_of(av[2]) << std::endl; 
-std::cout << "last_of       : " << data.find_last_of(av[2]) << std::endl;
-std::cout << "last_not_of   : " << data.find_last_not_of(av[2]) << std::endl; 
-*/
-/*
-    std::string test = "hello hello hello hello       f";
+void replaceAll(std::ifstream &ifs, std::ofstream &ofs, std::string src, std::string dst) {
+    std::string data;
 
-    std::cout << "length: " << test.length() << std::endl;
+    while (getline(ifs, data))
+    {
+        size_t pos = data.find(src);
 
-    int start = 0;
-    int end = 0;
-    while (start < test.length()) {
-
-        start = test.find("hello", start);
-        end = test.find_first_not_of("hello", start);
-
-        if (end < 0)
-            break ;
-
-        std::cout << test.substr(start, end) << "\n(start: " << start << "end: " << end << ")" << std::endl;
-        start = end;
+        while (pos != std::string::npos) {
+            data.erase(pos, src.length());
+            data.insert(pos, dst);
+            pos = data.find(src, pos + dst.length());
+        }
+        ofs << data;
+        if (!ifs.eof()) ofs << std::endl;
     }
 
-    std::cout << start << std::endl;
-*/
-
-                /* Ajust end_idx when start_idx is bigger than end_idx */
-                // for (int i = 0; start_idx > end_idx; i++)
-                //     end_idx = data.find_first_not_of(av[2], i);
+    ifs.close();
+    ofs.close();
+}
 
 int main(int ac, char *av[]) {
 
     if (ac == 4) {
 
-        std::string     output = av[1];
-        output += ".replace";
-
-        std::ifstream   ifs(av[1]);
-        std::ofstream   ofs(output);
-
-
-        if (ifs.is_open()) {
-            std::string data;
-            std::string replaced_word = av[3];
-            int         length = std::string(av[2]).length();
-
-            while (getline(ifs, data))
-            {
-                std::string tmp = "";
-                int         start_idx = 0;
-                int         end_idx = 0;
-                int         pivot = 0;
-
-                while (1) {
-
-                    start_idx = data.find(av[2], pivot);
-
-                    // printf("1/start_idx:%d pivot:%d\n", start_idx, pivot);
-                    tmp += data.substr(pivot, start_idx - pivot);
-                    tmp += replaced_word;
-                    pivot = data.find_first_not_of(av[2], start_idx);
-                    printf("2/%s(start_idx:%d pivot:%d)\n", tmp.c_str(), start_idx, pivot);
-                    if (pivot < 0) {
-                        break;
-                    }
-
-                }
-                std::cout << tmp << std::endl;
-
-                ofs << tmp << std::endl;
-            }
-
-            ifs.close();
-            ofs.close();
-
+        std::string     filename(av[1]);
+        std::ifstream   ifs(filename);
+        if (ifs.fail()) {
+            std::cout << "Error: fail to read file!" << std::endl;
+            return (-42);
         }
+        std::ofstream   ofs(filename + ".replace");
+        if (ofs.fail()) {
+            std::cout << "Error: fail to create file!" << std::endl;
+            return (-42);
+        }
+        std::string data;
+        std::string src(av[2]);
+        std::string dst(av[3]);
+        replaceAll(ifs, ofs, src, dst);
 
     } else std::cout << "USAGE: ./replace <filename> <word_to_replace> <replaced_word>" << std::endl;
+
     return (0);
 }
 
             
-                    // if (start_idx < data.length()) {
-                    //     std::string prev = data.substr(0, start_idx); 
-                    //     std::string next = data.substr(end_idx);
-                    // /* replaceAll */
-                    //     for (int i = 0; i < replaced_word.length(); i++)
-                    //         tmp += replaced_word[i];
-                    //     tmp = prev + tmp + next;
-                    // }
-                    // start_idx = end_idx;
-                    // printf("2start_idx:%d end_idx:%d\n", start_idx, end_idx);
