6ad19793461299098ac4324fe897c15667a754ba
jeanoza <choikb916@gmail.com>
2022-08-08 18:41:59 +0200

    feat(c02/ex0): first commit

---
+++ c02/ex00/Fixed.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 18:31:16 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/08/08 18:41:20 by kyubongchoi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+/* constructor */
+Fixed::Fixed(void) : _raw(0) {
+	std::cout << "Fixed: Default constructor called" << std::endl;
+}
+Fixed::Fixed(char raw) : _raw(raw) {
+	std::cout << "Fixed: [char] constructor called" << std::endl;
+}
+Fixed::Fixed(short raw) : _raw(raw) {
+	std::cout << "Fixed: [short] constructor called" << std::endl;
+}
+Fixed::Fixed(int raw) : _raw(raw) {
+	std::cout << "Fixed: [int] constructor called" << std::endl;
+}
+Fixed::Fixed(long raw) : _raw(raw) {
+	std::cout << "Fixed: [long] constructor called" << std::endl;
+}
+Fixed::Fixed(float raw) : _raw(raw) {
+	std::cout << "Fixed: [float] constructor called" << std::endl;
+}
+Fixed::Fixed(double raw) : _raw(raw) {
+	std::cout << "Fixed: [double] constructor called" << std::endl;
+}
+//FIXME: here
+Fixed::Fixed(Fixed & const i) {
+	std::cout << "Fixed: [Fixed] constructor called" << std::endl;
+}
+
+/* destructor */
+Fixed::~Fixed(void) {
+	std::cout << "Fixed: destructor called" << std::endl;
+	return ;
+}
+
+//FIXME:here
+/* operator */
+
+/* getter */
+int		Fixed::getRawBits(void) const{
+	return this->_raw;
+}
+
+/* setter */
+void	Fixed::setRawBits(int const raw) {
+	this->_raw = raw;
+}
---
+++ c02/ex00/Fixed.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 17:35:32 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/08/08 18:41:28 by kyubongchoi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# ifndef COLOR
+#  define RED "\033[0;31m"
+#  define GRN "\033[0;32m"
+#  define YEL "\033[0;33m"
+#  define BLU "\033[0;34m"
+#  define MAG "\033[0;35m" //purple
+#  define CYN "\033[0;36m" //emerald
+#  define WHT "\033[0;37m"
+#  define DFT "\033[0m"
+# endif
+
+# include <iostream>
+
+class Fixed {
+	private:
+		int				_raw;
+		static int const _bit;
+
+	public:
+		/* constructor */
+		Fixed(void);
+		Fixed(char raw);
+		Fixed(short raw);
+		Fixed(int raw);
+		Fixed(long raw);
+		Fixed(float raw);
+		Fixed(double raw);
+		Fixed(Fixed & i);
+
+		/* destructor */
+		~Fixed(void);
+
+		//FIXME:here
+		/* operator */
+
+		/* getter */
+		int getRawBits(void) const;
+
+		/* setter */
+		void setRawBits(int const raw);
+};
+#endif
