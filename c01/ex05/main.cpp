59a21564ba3f3805b4996d2c43ff66872bc68876
jeanoza <choikb916@gmail.com>
2022-08-08 14:19:08 +0200

    fix(c01/ex05): protect batise

--- c01/ex05/main.cpp
+++ c01/ex05/main.cpp
@@ -6,22 +6,25 @@
 /*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 14:03:18 by kyubongchoi       #+#    #+#             */
-/*   Updated: 2022/08/08 14:12:46 by kyubongchoi      ###   ########.fr       */
+/*   Updated: 2022/08/08 14:18:20 by kyubongchoi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main (void) {
-	Harl instance;
+int main (int ac, char *av[]) {
+	(void)av;
 
-	instance.complain("DEBUG");
-	instance.complain("INFO");
-	instance.complain("WARNING");
-	instance.complain("ERROR");
+	if (ac == 1) {
+		Harl instance;
 
-	/* test idiot */
-	instance.complain("N'import quoi");
+		instance.complain("DEBUG");
+		instance.complain("INFO");
+		instance.complain("WARNING");
+		instance.complain("ERROR");
 
+		/* test idiot */
+		instance.complain("N'import quoi");
+	} else std::cout << RED << "DO NOT DO STUPID THINGS!" << DFT << std::endl;
 	return (0);
 }
