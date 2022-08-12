253a3929aa8ab94ab0a82a816ba3ed3046369503
Kyubong CHOI <kychoi@e1r2p8.clusters.42paris.fr>
2022-08-12 14:09:32 +0200

    feat(c02/ex01)

---
+++ c02/ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kychoi <kychoi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 08:41:26 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/08/12 13:46:37 by kychoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	// std::cout << a.getRawBits() << std::endl;
+	// std::cout << b.getRawBits() << std::endl;
+	// std::cout << c.getRawBits() << std::endl;
+
+	/* thanks to operator overload, */
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+
+	/* test to verify if int const Fixed::_bit is always 8 */
+	std::cout << Fixed::getBit() << std::endl;
+	return 0;
+}
