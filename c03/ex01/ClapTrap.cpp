6b2b1df9615daeff83d35f99de576527acbb22b3
jeanoza <choikb916@gmail.com>
2022-08-19 13:14:24 +0200

    feat(c03/ex01): first commit

---
+++ c03/ex01/ClapTrap.cpp
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/15 18:23:51 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/08/19 13:13:13 by kyubongchoi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) :
+	_name(nullptr), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+	std::cout << "Default constructor called\n";
+	return ;
+}
+ClapTrap::ClapTrap(const std::string name)
+	: _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+	std::cout << "Parametric constructor called: " << this->_name << std::endl;
+	return;
+}
+ClapTrap::ClapTrap(const ClapTrap &inst) {
+	std::cout << "Copy constructor called\n";
+	*this = inst;
+	return ;
+}
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "Destructor called\n";
+	return;
+}
+ClapTrap &ClapTrap::operator=(const ClapTrap &rhs)
+{
+	std::cout << "Copy assignation operator called\n";
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_hitPoints = rhs._hitPoints;
+		this->_attackDamage = rhs._attackDamage;
+		this->_energyPoints = rhs._energyPoints;
+	}
+	return (*this);
+}
+
+/* getters */
+std::string	ClapTrap::getName(void) const {
+	return this->_name;
+}
+unsigned int	ClapTrap::getHitPoints(void) const{
+	return this->_hitPoints;
+}
+unsigned int	ClapTrap::getEnergyPoints(void) const{
+	return this->_energyPoints;
+}
+unsigned int	ClapTrap::getAttackDamage(void) const{
+	return this->_attackDamage;
+}
+
+/* member functions */
+void	ClapTrap::attack(const std::string &target) {
+	if (this->_energyPoints > 0 && this->_hitPoints > 0) {
+		std::cout << "ClapTrap " << this->_name << " attacks " << target
+			<< ", causing " << this->_attackDamage << " points of damage!\n";
+		--this->_energyPoints;
+	} else std::cout << RED << this->_name
+		<< " doesn't have enough energy or hit point to attack a cible!\n" << DFT;
+}
+void	ClapTrap::takeDamage(unsigned int amount) {
+	if (this->_hitPoints > 0) {
+		//Protection pour ne pas avoir underflow à this->_hitPoints
+		if (this->_hitPoints < amount) this->_hitPoints = 0;
+		else this->_hitPoints -= amount;
+		std::cout << "ClapTrap " << this->_name << " lost "
+			<< amount << " hit point (current: " << this->_hitPoints << ")\n" << DFT;
+	} else std::cout << RED << "ClapTrap " << this->_name << " is already dead!\n" << DFT;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount) {
+	if (this->_energyPoints > 0 && this->_hitPoints > 0) {
+		//Il n'y a pas de max_limit de _hitPoint dans le sujet, je n'ai pas à le gérér.
+		this->_hitPoints += amount;
+		--this->_energyPoints;
+		std::cout << "ClapTrap " << this->_name << " has been repaired with "
+				  << amount << " point.(current: " << this->_hitPoints << ")\n";
+	}
+	else std::cout << RED << this->_name 
+		<< " doesn't have enough energy or hit point to be repaired!\n" << DFT;
+}
+
+/* ostream overloading */
+std::ostream &operator<<(std::ostream &o, const ClapTrap &rhs) {
+	o << CYN << "\nState of ClapTrap: " << rhs.getName()
+	  << "\n _hitPoints: " << rhs.getHitPoints()
+	  << "\n _energyPoints: " << rhs.getEnergyPoints()
+	  << "\n _attackDamage: " << rhs.getAttackDamage()
+	  << DFT;
+
+	return o;
+}
---
+++ c03/ex01/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/15 18:23:56 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/08/19 12:47:44 by kyubongchoi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+#define COUNT_ATTACK 8
+#define COUNT_TAKE_D 3
+#define COUNT_REPAIR 3
+
+int main(void) {
+	ClapTrap a("hello");
+
+
+	std::cout << YEL << "\n\nTest attack:\n" << DFT;
+	for (int i = 0; i < COUNT_ATTACK; i++) a.attack("world");
+	std::cout << a << std::endl;
+	std::cout << YEL << "\n\nTest take damage:\n" << DFT;
+	for (int i = 0; i < COUNT_TAKE_D; ++i) a.takeDamage(3);
+	std::cout << a << std::endl;
+	std::cout << YEL << "\n\nTest repair:\n" << DFT;
+	for (int i = 0; i < COUNT_REPAIR; ++i) a.beRepaired(5);
+	std::cout << a << std::endl;
+
+	return (0);
+}
