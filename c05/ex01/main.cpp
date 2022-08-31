8e038f97d6c019f4fdc5453795dbd75ee99a0e10
jeanoza <choikb916@gmail.com>
2022-08-31 15:34:24 +0200

    feat(c05/ex01): first commit

---
+++ c05/ex01/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 11:51:06 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/08/31 15:22:52 by kyubongchoi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main(void) {
+	{
+		std::cout << "\n= = = = = TEST mandatory 1: exception at constructor = = = = =\n";
+		try {
+			Bureaucrat a("Jean", 3);
+			std::cout << a << std::endl;
+			Bureaucrat b("Paul", 151);
+			std::cout << b << std::endl;
+		} catch (std::exception &e) {
+			std::cout << RED << e.what() << DFT << std::endl;
+
+		}
+	}
+	{
+		std::cout << "\n= = = = = TEST mandatory 2: exception after at incrementGrade() = = = = =\n";
+		try {
+			Bureaucrat a("Jean", 3);
+			for (int i = 0; i < 4; ++i) {
+				a.incrementGrade();
+				std::cout << a << std::endl;
+			}
+			std::cout << a << std::endl;
+		} catch (std::exception &e) {
+			std::cout << RED << e.what() << DFT << std::endl;
+		}
+	}
+	{
+		std::cout << "\n= = = = = TEST mandatory 3: exception after at decrementGrade() = = = = =\n";
+		try {
+			Bureaucrat a("Paul", 148);
+			for (int i = 0; i < 4; ++i) {
+				a.decrementGrade();
+				std::cout << a << std::endl;
+			}
+			std::cout << a << std::endl;
+		} catch (std::exception &e) {
+			std::cout << RED << e.what() << DFT << std::endl;
+		}
+	}
+	{
+		std::cout << "\n= = = = = TEST dynamic allocation = = = = =\n";
+		Bureaucrat *a;
+		Bureaucrat *b;
+		try {
+			a = new Bureaucrat("Jean", 1);
+			std::cout << *a << std::endl;
+			delete a;
+			b = new Bureaucrat("Paul", -1);
+			// next 2 lines will not execute because of exception
+			std::cout << *b << std::endl;
+			delete b;
+		} catch (std::exception &e) {
+			std::cout << RED << e.what() << DFT << std::endl;
+		}
+	}
+	return 0;
+}
