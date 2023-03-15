66429bc63d239ef070d99ffffbdfb7e5f46efbb2
Kyubong CHOI <kychoi@student.42.fr>
2023-03-15 17:00:21 +0100

    feat(ex00): done

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <map>
+#include <ctime>
+
+# ifndef COLOR
+#  define RED "\033[0;31m"
+#  define GRN "\033[0;32m"
+#  define YEL "\033[0;33m"
+#  define BLU "\033[0;34m"
+#  define MAG "\033[0;35m" //purple
+#  define CYN "\033[0;36m" //emerald
+#  define WHT "\033[0;37m"
+#  define DFT "\033[0m"
+# endif
+
